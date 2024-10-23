8d070da042f6ac7e88ee1227b30e92034c257fa5
TÁO <tuanhaoggg@gmail.com>
2024-10-23 11:37:25 +0700

    video 30 jsx, fragment,css

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,6 +1,16 @@
+import "./style.css"
+// jsx - chi tra ve mot element
+// fragment (manh vo) - 
+
 const MyComponent = () => {
     return (
-        <div>Create application</div>
+        <>
+            <div>Create application</div>
+            <div className="myName" style={
+                { borderRadius: "10px" }
+            }
+            >My name</div>
+        </>
     );
 }
 
