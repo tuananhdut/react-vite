5b004f77e379c21f23e02b195c815ed01b3fe224
TÁO <tuanhaoggg@gmail.com>
2024-10-24 01:39:51 +0700

    video 36 - DOM Event

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -2,11 +2,26 @@ import "./todo.css"
 const TodoNew = (props) => {
     const { addNewTodo } = props
 
-    addNewTodo("Taos")
+    // addNewTodo("Taos")
+    const handleClick = () => {
+        alert("click me")
+    }
+    // const handleOnChange = (event) => {
+    //     console.log("handle on change", event.target.value)
+    // }
+    const handleOnChange = (name) => {
+        console.log("handle on change", name)
+    }
     return (
         <div className="todo-new">
-            <input type="text" name="inputTodo" />
-            <button>ADD</button>
+            <input type="text"
+                // onChange={handleOnChange}
+                onChange={(Event) => handleOnChange(Event.target.value)}
+            />
+            <button
+                onClick={handleClick}
+                style={{ cursor: "pointer" }}
+            >ADD</button>
         </div>
     )
 }
