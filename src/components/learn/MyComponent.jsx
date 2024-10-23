12444968c279492ae77a1a911df239429aba7c6c
TÁO <tuanhaoggg@gmail.com>
2024-10-23 13:01:37 +0700

    video 31-value number

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,14 +1,22 @@
 import "./style.css"
 // jsx - chi tra ve mot element
-// fragment (manh vo) - 
+// fragment (manh vo)
 
 const MyComponent = () => {
+    // const value = "Taos"
+    // const value = 25
+    // const value = null
+    // const value  = undefined
+    // const value = true
+    // const value = [1, 2, 3]
+    const value = {
+        name: "Taos",
+        age: 25
+    }
     return (
         <>
-            <div>Create application</div>
-            <div className="myName" style={
-                { borderRadius: "10px" }
-            }
+            <div>{JSON.stringify(value)} application</div>
+            <div className="myName" style={{ borderRadius: "10px" }}
             >My name</div>
         </>
     );
