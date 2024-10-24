509576a2c70d0b3b03b8007f7cdc73e5cf73d7fc
TÁO <tuanhaoggg@gmail.com>
2024-10-25 01:58:58 +0700

    video 69 create user by axios.post

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,6 +1,6 @@
 import { Input, Button } from 'antd';
 import { useState } from 'react';
-
+import axios, { Axios } from "axios"
 
 const UserFrom = () => {
     const [fullName, setFullName] = useState("")
@@ -10,6 +10,14 @@ const UserFrom = () => {
 
     // console.log({ fullName, email, password, phoneNumber })
     const handleOnClick = () => {
+        const URL_BACKEND = "http://localhost:8080/api/v1/user"
+        const data = {
+            fullName: fullName,
+            email: email,
+            password: password,
+            phone: phoneNumber
+        }
+        axios.post(URL_BACKEND, data)
         console.log({ fullName, email, password, phoneNumber })
     }
 
