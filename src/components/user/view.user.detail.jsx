44f81774fe22dad70625d208d1fadd666e7e8608
TÁO <tuanhaoggg@gmail.com>
2024-10-29 13:05:16 +0700

    video 83 preview imager

--- src/components/user/view.user.detail.jsx
+++ src/components/user/view.user.detail.jsx
@@ -1,8 +1,24 @@
 import { Button, Drawer } from 'antd';
+import { useState } from 'react';
 
 const ViewUserDetail = (props) => {
     const { isViewUserDetail, setIsViewUserDetail, dataUserDetail, setDataUserDetail } = props
+    const [selectFile, setSelectFile] = useState(null)
+    const [preview, setPreview] = useState(null);
 
+
+    const handleOnchageFile = (event) => {
+        if (!event.target.files || event.target.files.length === 0) {
+            setSelectFile(null)
+            setPreview(null)
+            return;
+        }
+        const file = event.target.files[0]
+        if (file) {
+            setSelectFile(file)
+            setPreview(URL.createObjectURL(file))
+        }
+    }
     return (
         <Drawer
             width="40vw"
@@ -23,7 +39,13 @@ const ViewUserDetail = (props) => {
                     <br />
                     <p>Avatar :</p>
                     <div>
-                        <img style={{ width: "300px", padding: "8px 0" }} src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${dataUserDetail.avatar}`} />
+                        <img
+                            style={{
+                                width: "250px",
+                                margin: "8px 0",
+                                border: "1px solid #ccc"
+
+                            }} src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${dataUserDetail.avatar}`} />
                     </div>
                     <div>
                         <label htmlFor="btmUpload"
@@ -34,8 +56,17 @@ const ViewUserDetail = (props) => {
                                 background: "orange",
                                 padding: "5px 10px"
                             }}>Upload Avatar</label>
-                        <input type="file" hidden id='btmUpload' />
+                        <input type="file" hidden id='btmUpload' onChange={(event) => handleOnchageFile(event)} />
                     </div>
+                    {preview && <div>
+                        <img style={{
+                            width: "250px",
+                            margin: "8px 0",
+                            border: "1px solid #ccc"
+                        }} src={preview} />
+                    </div>}
+
+
                 </> : <>
                     <p>Không có dữ liệu</p>
                 </>}
